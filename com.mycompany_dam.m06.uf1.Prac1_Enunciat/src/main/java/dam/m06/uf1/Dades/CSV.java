d2a983fb5ed4df412729b1414e82da1b8e017f2a
paredes.llerena.leandro.joseph <URCqNeZR>
2021-02-22 20:53:09 +0100

    initial commit

---
+++ com.mycompany_dam.m06.uf1.Prac1_Enunciat/src/main/java/dam/m06/uf1/Dades/CSV.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam.m06.uf1.Dades;
+
+import dam.m06.uf1.Aplicacio.Model.Equips;
+import java.io.File;
+
+/**
+ *
+ * @author manel
+ */
+public class CSV {
+    
+    /**
+     * Exporta tots els equips a un fitxer CSV
+     * @param fitx
+     * @param dades
+     * @throws DadesException 
+     */
+    public static void exportaEquipsACSV(File fitx, Equips dades) throws DadesException
+    {
+        throw new DadesException("No implementat");
+    }
+    
+    /**
+     * Exporta tots els jugadors a un fitxer CSV
+     * @param fitx
+     * @param dades
+     * @throws DadesException 
+     */
+    public static void exportaJugadorsACSV(File fitx, Equips dades) throws DadesException
+    {
+      throw new DadesException("No implementat");
+    }
+}
